d3980c661bf90a5ec4ecd906397f7db5a4d1ccbc
Paul Wu <linfei19888@sina.com>
2019-07-25 08:53:16 +0800

    init

---
+++ server.js
@@ -0,0 +1,23 @@
+
+const http = require('http');
+
+var hostname = '127.0.0.1';
+var port = 3000;
+
+hostname = process.env.IP;
+port = process.env.PORT;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+
+
+
